7e4f38671452c23acec373d611046894c6736974
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-25 13:57:05 +0100

    slack 3

--- javascript.js
+++ javascript.js
@@ -89,3 +89,80 @@ voto70Id120 = studentiUpperCase.filter((el) =>{return el.voto > 70 && el.id > 12
 console.log(studentiUpperCase);
 console.log('array con solo voto > 70:', voto70);
 console.log('array con voto > 70 e ID >120:', voto70Id120);
+
+
+/*SNACK 3
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando destructuring e template literal */
+
+
+function weightRandom(){
+     
+
+    let weight = (Math.random() * 10).toFixed(2);
+
+    return weight;
+
+
+}
+
+
+
+
+let bicicle = [
+    {
+        model:'Atala',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Bianchi',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Bottecchia',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Piaggio',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Pinarello',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Garelli',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Basso',
+        weight: parseFloat(weightRandom())
+    },
+    {
+        model:'Carrera',
+        weight: parseFloat(weightRandom())
+    },
+
+]
+
+
+
+let lightestBike = bicicle[0]
+
+
+for (let i = 1; i < bicicle.length; i++){
+    
+    if (bicicle[i].weight < lightestBike.weight){
+        lightestBike = bicicle[i]
+        
+    }
+
+    
+}
+
+const {model,weight} = lightestBike
+
+console.log(bicicle)
+console.log(lightestBike)
+console.log(`La piu' leggera e' la ${model} con un peso di ${weight}kg`)
